c9737f682b84c654eebaf0605421bbaf4edacb6c
KevinInnes <niveksenni@gmail.com>
2025-06-10 08:31:44 +0200

    Just trying to align

--- createDB.py
+++ createDB.py
@@ -1,7 +1,6 @@
 
 import pandas as pd
-#import duckdb
-
+import duckdb
 
 # Define the column specifications (widths) and names
 colspecs =     [(0, 6),(6, 15),(15, 23),(23, 29),(29, 35),(35, 36),(36, 42),(42, 49),(49, 50),(50, 59),(59, 68),(68, 77),(77, 86),(86, 95),(95, 104),(104, 113),
@@ -17,18 +16,17 @@ widths =[7,8,8,6,6,1,6,7,1,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,2,3,10,10
 fileName= r'NSPL21_NOV_2023_UK.txt'
 
 # Read the fixed-width text file
-df = pd.read_fwf(fileName, widths=widths, names=column_names)
+#df = pd.read_fwf(fileName, widths=widths, names=column_names)
 
 # Connect to a database file (this will create the file if it doesn't exist)
 conn = duckdb.connect('UK-Census-DB.duckdb')
 
 # Create the table and insert data from the DataFrame
-conn.execute("""
-CREATE TABLE postcode_lookup AS SELECT * FROM df
-""")
+sql = "CREATE TABLE postcode_lookup AS SELECT * FROM df"
+conn.execute(sql)
 
 # Verify the data
-result = conn.execute("SELECT * FROM postcode_lookup").fetchall()
+result = conn.execute("SELECT * FROM postcode_lookup limit 1000").fetchall()
 print(result)
 
 # Close the connection
